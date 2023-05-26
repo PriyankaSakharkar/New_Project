083fb5ffa303372d4014ca693ab806881f61fe82
PriyankaSakharkar <priyanka@sakharkar>
2023-05-26 16:24:06 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,29 @@ function App() {
        <Add a= '8' b= '2'></Add>
        <Sub a= '5' b= '2'/>
 
+       <table>
+  <tr>
+    <th>Name</th>
+    <th>Collage</th>
+    <th>City</th>
+  </tr>
+  <tr>
+    <td>Priyanka Sakharkar</td>
+    <td>KDK</td>
+    <td>Nagpur</td>
+  </tr>
+  <tr>
+    <td>Neha Likhar</td>
+    <td>NIT</td>
+    <td>Nagpur</td>
+  </tr>
+  <tr>
+    <td>Vaishali Barai</td>
+    <td>NIT</td>
+    <td>Nagpur</td>
+  </tr>
+</table>
+
     </div>
   );
 }
